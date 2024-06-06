3f74a1aedd7665e0367fb89d56a2bd80cdd23b27
Vitor Reis <reis_vitor@outlook.com>
2024-06-06 04:52:03 -0300

    first commit

---
+++ src/main/java/com/example/sqlinjectiondemo/SqlinjectiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlinjectiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlinjectiondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlinjectiondemoApplication.class, args);
+	}
+
+}
